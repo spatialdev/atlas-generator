5c22bca107bfb2dbfad3e909147726a70d15fea6
Bentley Breithaupt <bentleysb@gmail.com>
2018-05-24 18:49:36 -0700

    Updated atlas version used, added way sectioning.

--- src/main/java/PbfToAtlas.java
+++ src/main/java/PbfToAtlas.java
@@ -1,12 +1,21 @@
+import org.openstreetmap.atlas.geography.MultiPolygon;
+import org.openstreetmap.atlas.geography.atlas.pbf.AtlasLoadingOption;
+import org.openstreetmap.atlas.geography.boundary.CountryBoundaryMap;
 import org.openstreetmap.atlas.streaming.resource.File;
 import org.openstreetmap.atlas.geography.atlas.pbf.OsmPbfLoader;
+
+import java.util.Collections;
 
 public class PbfToAtlas
 {
     private final OsmPbfLoader atlas;
+    private final static MultiPolygon polygon = MultiPolygon.MAXIMUM;
 
     public PbfToAtlas(File pbf) {
-        this.atlas = new OsmPbfLoader(pbf);
+
+        final CountryBoundaryMap map = CountryBoundaryMap.fromBoundaryMap(Collections.singletonMap("UNK", this.polygon));
+        final AtlasLoadingOption option = AtlasLoadingOption.createOptionWithAllEnabled(map);
+        this.atlas = new OsmPbfLoader(pbf, this.polygon, option);
     }
 
     public void save(File out){
